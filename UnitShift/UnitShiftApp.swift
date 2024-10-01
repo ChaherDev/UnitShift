a148febb30162a88e3028bdeba4ca3a5c0b6d085
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-01 18:11:19 +0200

    Initial Commit

---
+++ UnitShift/UnitShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitShiftApp.swift
+//  UnitShift
+//
+//  Created by Chaher Machhour on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
